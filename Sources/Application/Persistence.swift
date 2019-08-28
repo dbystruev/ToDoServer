38576157134724da920bba5efc1f2d149b8a5669
dbystruev <github@learnswift.ru>
2019-08-28 23:44:46 +0300

    Adapted for running in Docker

--- Sources/Application/Persistence.swift
+++ Sources/Application/Persistence.swift
@@ -5,15 +5,20 @@
 //  Created by Denis Bystruev on 23/08/2019.
 //
 
+import Foundation
 import SwiftKueryORM
 import SwiftKueryPostgreSQL
 
 class Persistence {
     static func setUp() {
         let pool = PostgreSQLConnection.createPool(
-            host: "localhost",
+            host: "postgresql-database",
             port: 5432,
-            options: [.databaseName("tododb")],
+            options: [
+                .databaseName("tododb"),
+                .userName("postgres"),
+                .password(ProcessInfo.processInfo.environment["DBPASSWORD"] ?? "nil"),
+            ],
             poolOptions: ConnectionPoolOptions(initialCapacity: 10, maxCapacity: 50)
         )
         Database.default = Database(pool)
